882ded7f72bc0120750ce211fdcd4e13661a566f
DANIIL <danya.raev.96@mail.ru>
2023-11-12 12:21:23 +0300

    The final

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,30 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import {getJoke} from "./axios/getJoke"
-import CardJoke from "./component/card/Card"
-import {IJoke} from "./interfaces/IJoke"
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { getJoke } from "./axios/getJoke";
+import CardJoke from "./component/card/Card";
+import Buttons from "./component/button/Button";
+import { IJoke } from "./interfaces/IJoke";
+import "./App.css";
 
 export type TJokeList = IJoke;
 
 function App() {
+  const [jokes, setJokes] = useState<TJokeList>();
+  const [next, setNext] = useState<boolean>(false);
 
-   const [jokes ,setJokes] = useState<TJokeList>();
-   const [next,setNext]=useState<boolean>(false);
+  function callBack() {
+    return setNext(!next);
+  }
 
   useEffect(() => {
-      getJoke().then((response : any) => {
-        console.log(response.data)
-        setJokes(response.data)
-      }).catch((error)=>{
-        console.log(error)
+    getJoke()
+      .then((response: any) => {
+        console.log(response.data);
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }, [next]);
-  
+
   return (
-    <div>
-      <CardJoke items={jokes!}/>
-      <button onClick={()=>{setNext(!next)}}>next</button>
-    </div>
+    <>
+      <CardJoke items={jokes!} />
+      <Buttons state={callBack} />
+    </>
   );
 }
 
--- my-app/src/component/card/Card.tsx
+++ my-app/src/component/card/Card.tsx
@@ -1,30 +1,36 @@
-import React,{useState} from "react";
-import {TJokeList} from "../../App"
+import React, { useState } from "react";
+import { TJokeList } from "../../App";
 import { Box } from "@mui/material";
-import "../card/card.css"
+import Tooltip from "@mui/material/Tooltip";
+import "../card/card.css";
 
+interface JokeListProps {
+  items: TJokeList;
+}
 
-
-  interface JokeListProps {
-    items: TJokeList;
-  }
-
- const CardJoke: React.FC<JokeListProps> = (items) => {
+const CardJoke: React.FC<JokeListProps> = (items) => {
   const [activeState, setActiveState] = useState<boolean>(false);
-    return(
-      <Box className="col-lg-3 col-md-3 col-sm-3 col-xs-6 cardContainer">
-      <Box onClick={()=>{setActiveState(prev => !prev)}} className={`card ${activeState ? 'flipped' : ' '}`}>
-        <Box className="front">{items.items?.setup}</Box>
-        <Box className="back">
-          <Box className="content">
-            <h3 className="cardTitle">{items.items?.punchline}</h3>
-            <br/>
-            <p id="laugh"></p>
+  return (
+    <Box className="col-lg-3 col-md-3 col-sm-3 col-xs-6 cardContainer">
+      <Tooltip title={"Click"}>
+        <Box
+          onClick={() => {
+            setActiveState((prev) => !prev);
+          }}
+          className={`card ${activeState ? "flipped" : " "}`}
+        >
+          <Box className="front">{items.items?.setup}</Box>
+          <Box className="back">
+            <Box className="content">
+              <h3 className="cardTitle">{items.items?.punchline}</h3>
+              <br />
+              <p id="laugh"></p>
+            </Box>
           </Box>
         </Box>
-      </Box>
+      </Tooltip>
     </Box>
-    )
-}
+  );
+};
 
 export default CardJoke;
